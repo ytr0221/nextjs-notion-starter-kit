7988d6f232fdde93ec06e63c1fc6eae7d6008a47
ytr0221 <101045624+ytr0221@users.noreply.github.com>
2022-03-09 15:35:41 +0900

    Update NotionPage.tsx

--- components/NotionPage.tsx
+++ components/NotionPage.tsx
@@ -9,7 +9,7 @@ import BodyClassName from 'react-body-classname'
 //import useDarkMode from 'use-dark-mode'
 import { PageBlock } from 'notion-types'
 
-import { Tweet, TwitterContextProvider } from 'react-static-tweets'
+//import { Tweet, TwitterContextProvider } from 'react-static-tweets'
 
 // core notion renderer
 import { NotionRenderer, Code, Collection, CollectionRow } from 'react-notion-x'
@@ -19,7 +19,7 @@ import { getBlockTitle } from 'notion-utils'
 import { mapPageUrl, getCanonicalPageUrl } from 'lib/map-page-url'
 import { mapNotionImageUrl } from 'lib/map-image-url'
 import { getPageDescription } from 'lib/get-page-description'
-import { getPageTweet } from 'lib/get-page-tweet'
+//import { getPageTweet } from 'lib/get-page-tweet'
 import { searchNotion } from 'lib/search-notion'
 import * as types from 'lib/types'
 import * as config from 'lib/config'
@@ -29,7 +29,7 @@ import { CustomFont } from './CustomFont'
 import { Loading } from './Loading'
 import { Page404 } from './Page404'
 import { PageHead } from './PageHead'
-import { PageActions } from './PageActions'
+//import { PageActions } from './PageActions'
 import { Footer } from './Footer'
 //import { PageSocial } from './PageSocial'
 //import { GitHubShareButton } from './GitHubShareButton'
@@ -41,7 +41,7 @@ import styles from './styles.module.css'
  const Code = dynamic(() =>
    import('react-notion-x').then((notion) => notion.Code)
  )
-
+*/
  const Collection = dynamic(() =>
    import('react-notion-x').then((notion) => notion.Collection)
  )
@@ -51,7 +51,7 @@ import styles from './styles.module.css'
    {
      ssr: false
    }
- )*/
+ )
 
 // TODO: PDF support via "react-pdf" package has numerous troubles building
 // with next.js
@@ -144,7 +144,7 @@ export const NotionPage: React.FC<types.PageProps> = ({
   let pageAside: React.ReactChild = null
 
   // only display comments and page actions on blog post pages
-  if (isBlogPost) {
+  /*if (isBlogPost) {
     if (config.utterancesGitHubRepo) {
       comments = (
         <ReactUtterances
@@ -162,10 +162,10 @@ export const NotionPage: React.FC<types.PageProps> = ({
     }
   } else {
     pageAside = <></>//<PageSocial />
-  }
+  }*/
 
   return (
-    <TwitterContextProvider
+   /* <TwitterContextProvider
       value={{
         tweetAstMap: (recordMap as any).tweetAstMap || {},
         swrOptions: {
@@ -173,17 +173,18 @@ export const NotionPage: React.FC<types.PageProps> = ({
             fetch(`/api/get-tweet-ast/${id}`).then((r) => r.json())
         }
       }}
-    >
+    >*/
       <PageHead site={site} />
 
       <Head>
+       {/*
         <meta property='og:title' content={title} />
         <meta property='og:site_name' content={site.name} />
 
         <meta name='twitter:title' content={title} />
         <meta property='twitter:domain' content={site.domain} />
 
-        {/* config.twitter && (
+         config.twitter && (
           <meta name='twitter:creator' content={`@${config.twitter}`} />
         )}
 
@@ -281,7 +282,7 @@ export const NotionPage: React.FC<types.PageProps> = ({
         }*/
       />
 
-      {/* <GitHubShareButton /> */}
-    </TwitterContextProvider>
+      {/* <GitHubShareButton /> 
+    </TwitterContextProvider>*/}
   )
 }
